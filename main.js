4eebfa6e4596dbcc4807ef06765348f5ea20b290
James Boudouris <JamesBoudouris@Georges-MacBook-Pro.local>
2018-06-05 20:17:10 -0500

    added gitnore and fixed canvas

--- main.js
+++ main.js
@@ -5,17 +5,17 @@ const LIMIT = 1;
 const RATING = 'pg';
 
 
-var phrases = ["me after a long day of ruining my life", "when you in the shower and the lights flicker", "when you pay the minimum on your credit card", "me after launching at my own jokes", "when you wait for the waiter you become the waiter", "when someone makes you take out your earphones to say something pointless", "when your girls says we need to talk & every single thing you’ve done since you’ve been together crosses your mind", "when you’re eating fistfuls of cheese at 3am and someone turns on the lights", "TFW when you find out Wisconsinites are made of 70% cheese", "when you learn C++ why not A++", "How’s your degree going?", "when you delete a block of code you thought was useless", "Me sneaking snacks into the theatre", "when you realise you slept through all the alarms for your 7am exam", "Me reading your message previews from the pull down bar on my phone and not answering for the next 6 hours", "when your mom says stop playing Minecraft and you say NO", "when you drop your phone but the screen doesn’t crack", "me on FaceTime ignoring everything the person is saying and looking at myself", "me trying to sleep at a decent time every night", "5th graders when they jump up and touch the top of the doorway", "when your friend in the backseat has AUX privileges and they start playing Nickelback", "when someone tries to call me instead of texting", "my face when I type LMFAOOO", "when it’s december 31st and someone says see you next year", "when you wake up at 9am on a weekend", "when you match with someone on tinder as soon as you download it"]
+var phrases = ["me after a long day of ruining my life", "when you in the shower and the lights flicker", "when you pay the minimum on your credit card", "me after launching at my own jokes", "when you wait for the waiter you become the waiter", "when someone makes you take out your earphones to say something pointless", "when your girls says we need to talk & every single thing you’ve done since you’ve been together crosses your mind", "when you’re eating fistfuls of cheese at 3am and someone turns on the lights", "TFW when you find out Wisconsinites are made of 70% cheese", "when you learn C++ why not A++", "How’s your degree going?", "when you delete a block of code you thought was useless", "Me sneaking snacks into the theatre", "when you realise you slept through all the alarms for your 7am exam", "Me reading your message previews from the pull down bar on my phone and not answering for the next 6 hours", "when your mom says stop playing Minecraft and you say NO", "when you drop your phone but the screen doesn’t crack", "me on FaceTime ignoring everything the person is saying and looking at myself", "me trying to sleep at a decent time every night", "5th graders when they jump up and touch the top of the doorway", "when your friend in the backseat has AUX privileges and they start playing Nickelback", "when someone tries to call me instead of texting", "my face when I type LMFAOOO", "when it’s december 31st and someone says see you next year", "when you wake up at 9am on a weekend", "when you match with someone on tinder as soon as you download it"];
 
 var curPlayer = 0;
 var curJudge = 1;
 
-var player0cards = new Array();
-var player1cards = new Array();
-var player2cards = new Array();
-var player3cards = new Array();
+var player0cards = [];
+var player1cards = [];
+var player2cards = [];
+var player3cards = [];
 
-var choosenCards = new Array();
+var choosenCards = [];
 
 var player0score = 0;
 var player1score = 0;
@@ -43,7 +43,7 @@ function getGif() {
   xhr.open("GET", "https://" + BASE_URL + ENDPOINT + "&api_key=" + PUBLIC_KEY +"&tag=&rating=PG", false);
   xhr.send();
   var myArr = JSON.parse(xhr.responseText);
-  var gifUrl = myArr.data.images.original.url
+  var gifUrl = myArr.data.images.original.url;
   return gifUrl;
 }
 
@@ -52,7 +52,7 @@ function getRandomInt(max) {
 }
 
 function randPhrase() {
-    var index = getRandomInt(phrases.length)
+    var index = getRandomInt(phrases.length);
     document.getElementById('currentPhrase').innerHTML = phrases[index];
     return phrases[index];
 }
@@ -125,10 +125,10 @@ function findSelected(selectedURL) {
 }
 
 function switchCards(nextPlayer) {
-  document.getElementById("cards1").src = nextPlayer[0]
-  document.getElementById("cards2").src = nextPlayer[1]
-  document.getElementById("cards3").src = nextPlayer[2]
-  document.getElementById("cards4").src = nextPlayer[3]
+  document.getElementById("cards1").src = nextPlayer[0];
+  document.getElementById("cards2").src = nextPlayer[1];
+  document.getElementById("cards3").src = nextPlayer[2];
+  document.getElementById("cards4").src = nextPlayer[3];
   document.getElementById("cards5").src = nextPlayer[4]
 }
 
@@ -177,7 +177,7 @@ function cardChoosen(selected) {
   } else if (selected == "choice3") {
     player3score++;
   }
-    resetChoosen()
+    resetChoosen();
     randPhrase()
   }
 
--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 var socket = io();
-socket.on('message', function(data) {
-  console.log(data);
+socket.on('message', function (data) {
+    console.log(data);
 });
 
 // tracks the current scores of each player, starts at 0
@@ -11,10 +11,10 @@ var player4Score = 0;
 
 // sets the displayed values to the players current scores
 function showScores() {
-  document.getElementById("p1score").innerHTML = player1Score;
-  document.getElementById("p2score").innerHTML = player2Score;
-  document.getElementById("p3score").innerHTML = player3Score;
-  document.getElementById("p4score").innerHTML = player4Score;
+    document.getElementById("p1score").innerHTML = player1Score;
+    document.getElementById("p2score").innerHTML = player2Score;
+    document.getElementById("p3score").innerHTML = player3Score;
+    document.getElementById("p4score").innerHTML = player4Score;
 }
 
 // 
